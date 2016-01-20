904c293b5736b2087fe1b7c3b4f1c38d33fd1c92
Michael Sumner <mdsumner@gmail.com>
2016-01-20 21:30:26 +1100

    add Shane's description

---
+++ R/ShinyrAtlantis-package.r
@@ -0,0 +1,7 @@
+#' ShinyrAtlantis.
+#' sh.prm   displays data stored in a .prm file
+#' sh.init   displays data stored in the .nc file containing initial values
+#' sh.dist  generates horizontal probability distributions that can be cut-and-pasted into a .prm file
+#' @name ShinyrAtlantis
+#' @docType package
+NULL
