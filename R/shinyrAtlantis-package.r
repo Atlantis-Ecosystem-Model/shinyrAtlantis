5b1a52ac12ec0f16ebff26eb7a2a59a7830d5f10
Michael Sumner <mdsumner@gmail.com>
2016-01-22 11:23:06 +1100

    consistent case

--- R/shinyrAtlantis-package.r
+++ R/shinyrAtlantis-package.r
@@ -1,7 +1,7 @@
-#' ShinyrAtlantis.
+#' shinyrAtlantis.
 #' sh.prm   displays data stored in a .prm file
 #' sh.init   displays data stored in the .nc file containing initial values
 #' sh.dist  generates horizontal probability distributions that can be cut-and-pasted into a .prm file
-#' @name ShinyrAtlantis
+#' @name shinyrAtlantis
 #' @docType package
 NULL
