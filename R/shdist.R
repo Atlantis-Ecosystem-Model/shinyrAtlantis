d5b0bcd3e778c37aa0c14ea85ee5847b52ffb1d7
Michael Sumner <mdsumner@gmail.com>
2016-02-10 12:41:56 +1100

    coord_cartesian

--- R/shdist.R
+++ R/shdist.R
@@ -16,7 +16,7 @@
 #' @return object of class 'shiny.appobj' see \code{\link[shiny]{shinyApp}}
 #' @importFrom dplyr mutate
 #' @importFrom stringr str_split
-#' @importFrom ggplot2 ggplot aes element_blank geom_polygon geom_text labs scale_fill_gradient scale_fill_manual scale_x_continuous scale_y_continuous theme theme_bw  xlab ylab
+#' @importFrom ggplot2 ggplot aes coord_cartesian element_blank geom_polygon geom_text labs scale_fill_gradient scale_fill_manual scale_x_continuous scale_y_continuous theme theme_bw  xlab ylab
 #' @examples
 #' \dontrun{
 #' prm.object <- make.dist.object(bgm.file)
